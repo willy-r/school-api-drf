419c4d8d1b17111ea374c3c7bda364db8a0c4569
William Rodrigues <williamrodrigues2442@gmail.com>
2022-04-14 18:23:45 -0300

    feat: install django-rest-framework

--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Thirty party apps.
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
