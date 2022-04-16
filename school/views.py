6739ce4d759308cc34d05323937fa8755d8d6223
William Rodrigues <williamrodrigues2442@gmail.com>
2022-04-16 17:24:19 -0300

    feat: add basic authentication

--- school/views.py
+++ school/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from school.models import Student, Course, Enrollment
 from school import serializers
@@ -8,23 +10,31 @@ class StudentViewSet(viewsets.ModelViewSet):
     """Viewset for students."""
     queryset = Student.objects.all()
     serializer_class = serializers.StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     """Viewset for courses."""
     queryset = Course.objects.all()
     serializer_class = serializers.CourseSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
     """Viewset for enrollments."""
     queryset = Enrollment.objects.all()
     serializer_class = serializers.EnrollmentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class ListStudentEnrollments(generics.ListAPIView):
     """Lists student enrollments."""
     serializer_class = serializers.ListStudentEnrollmentsSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Returns queryset with all enrollments of a student."""
@@ -35,6 +45,8 @@ class ListStudentEnrollments(generics.ListAPIView):
 class ListCourseStudents(generics.ListAPIView):
     """Lists all students of a course."""
     serializer_class = serializers.ListCourseStudentsSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Returns queryset with all students of a course."""
